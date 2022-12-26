d616395e80b9619816a04a99e5dfff3aefbabb80
Mintesnot Fikir <96992238+mintesnot96@users.noreply.github.com>
2022-12-26 21:31:55 +0300

    Create python-print-hackerrank3.py
    # question link
    # https://www.hackerrank.com/challenges/python-print/problem?isFullScreen=true

---
+++ python-print-hackerrank3.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    m = int(input())
+    n = ''
+    for i in range(1, m + 1):
+        n =n+str(i)
+    print(n)
+# question link
+# https://www.hackerrank.com/challenges/python-print/problem?isFullScreen=true
