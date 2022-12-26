1334154ad253e51221e57f5a41966556c92861e7
Mintesnot Fikir <96992238+mintesnot96@users.noreply.github.com>
2022-12-26 20:45:25 +0300

    Create python-division-hackerrank2.py
    #  question link
    #  https://www.hackerrank.com/challenges/python-division/problem?isFullScreen=true

---
+++ python-division-hackerrank2.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
+    
+# question link
+#  https://www.hackerrank.com/challenges/python-division/problem?isFullScreen=true
