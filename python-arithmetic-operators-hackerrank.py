24c644ed77fc7eeae88699aa66fbefd6c0d23cd4
Mintesnot Fikir <96992238+mintesnot96@users.noreply.github.com>
2022-12-26 20:41:14 +0300

    Create python-arithmetic-operators-hackerrank.py
    # question link
    # https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true

---
+++ python-arithmetic-operators-hackerrank.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    
+    
+# question link
+# https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true
+   
