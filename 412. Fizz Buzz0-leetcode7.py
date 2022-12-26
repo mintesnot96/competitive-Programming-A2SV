4c2ccad0be92b8f2465e86d802bebded9f0f0251
Mintesnot Fikir <96992238+mintesnot96@users.noreply.github.com>
2022-12-26 21:14:46 +0300

    Create 412. Fizz Buzz0-leetcode7.py
    # question link
    # https://leetcode.com/problems/fizz-buzz/description/

---
+++ 412. Fizz Buzz0-leetcode7.py
@@ -0,0 +1,15 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        answer=[]
+        for i in range(1,n+1):
+            if i%15==0:
+                answer.append('FizzBuzz')
+            elif i%3:
+                answer.append('Fizz')
+            elif i%5==0:
+                answer.append('Buzz')
+            else:
+                answer.append(str(i))     
+        return answer
+# question link
+# https://leetcode.com/problems/fizz-buzz/description/
