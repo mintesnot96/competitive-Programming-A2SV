88bae06ff83123b77b1ff84d3440515a069bfb13
Mintesnot Fikir <96992238+mintesnot96@users.noreply.github.com>
2022-12-29 18:55:51 +0300

    Create python-string-split-and-join-hackerrank5.py
    # question link
    # https://www.hackerrank.com/challenges/python-string-split-and-join/problem?isFullScreen=true
    In Python, a string can be split on a delimiter.
    Joining a string is simple:
    
    >>> a = "-".join(a)
    >>> print a
    this-is-a-string 
    
    >>> a = "this is a string"
    >>> a = a.split(" ") # a is converted to a list of strings. 
    >>> print a
    ['this', 'is', 'a', 'string']

---
+++ python-string-split-and-join-hackerrank5.py
@@ -0,0 +1,14 @@
+def split_and_join(line):
+    # write your code here
+    line=line.replace(" ",'-')
+    return line
+#or
+# line=line.split(" ")
+# line='-'.join(line)
+# return line
+# or
+# line=line.split(" ")
+# return '-'.join(line)
+
+# question link
+# https://www.hackerrank.com/challenges/python-string-split-and-join/problem?isFullScreen=true
