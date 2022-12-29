d2c381efa52d6f990441927f2d8bdf88b49ef618
Mintesnot Fikir <96992238+mintesnot96@users.noreply.github.com>
2022-12-29 19:07:03 +0300

    Update whats-your-name-hackerrank6.py

--- whats-your-name-hackerrank6.py
+++ whats-your-name-hackerrank6.py
@@ -1,12 +1,3 @@
-#
-# Complete the 'print_full_name' function below.
-#
-# The function is expected to return a STRING.
-# The function accepts following parameters:
-#  1. STRING first
-#  2. STRING last
-#
-
 def print_full_name(first, last):
     # Write your code here
     return print(f'Hello {first} {last}! You just delved into python.')
