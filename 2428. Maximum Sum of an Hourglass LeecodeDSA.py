f470d2c20380adf03610a74c7ad8aaab7e417150
Mintesnot Fikir <96992238+mintesnot96@users.noreply.github.com>
2023-02-01 15:34:41 +0300

    Create 2428. Maximum Sum of an Hourglass LeecodeDSA.py

---
+++ 2428. Maximum Sum of an Hourglass LeecodeDSA.py
@@ -0,0 +1,12 @@
+class Solution:
+  def maxSum(self, grid: List[List[int]]) -> int:
+    return max(grid[i - 1][j - 1] + grid[i - 1][j] + grid[i - 1][j + 1] + grid[i][j] +
+               grid[i + 1][j - 1] + grid[i + 1][j] + grid[i + 1][j + 1]
+               for i in range(1, len(grid) - 1)
+               for j in range(1, len(grid[0]) - 1))
+
+
+            # space = n
+            # time  = nm
+
+# https://leetcode.com/problems/maximum-sum-of-an-hourglass/
