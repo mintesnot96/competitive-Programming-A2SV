a896b4e7e13035c9bac307d7d22f679214e8dfe1
Mintesnot Fikir <96992238+mintesnot96@users.noreply.github.com>
2023-04-01 19:40:28 +0300

    Create 1963. Minimum Number of Swaps to M206. Reverse Linked List leetcode DSA.py

---
+++ 1963. Minimum Number of Swaps to M206. Reverse Linked List leetcode DSA.py
@@ -0,0 +1,17 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev = None
+        # 1 2 3 4 5 6
+        while head is not None:
+            next_node = head.next
+            head.next = prev
+            prev = head
+            head = next_node
+        return prev
+
+https://leetcode.com/problems/reverse-linked-list/
