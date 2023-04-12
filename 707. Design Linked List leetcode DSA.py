3f394bc9dea2488c4aeb1c6b6da97be496a5d46a
Mintesnot Fikir <96992238+mintesnot96@users.noreply.github.com>
2023-04-12 16:40:00 +0300

    Create 707. Design Linked List leetcode DSA.py

---
+++ 707. Design Linked List leetcode DSA.py
@@ -0,0 +1,67 @@
+class MyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    def get(self, index: int) -> int:
+        if index < 0 or index >= self.size:
+            return -1
+        curr = self.head
+        for i in range(index):
+            curr = curr.next
+        return curr.val
+
+    def addAtHead(self, val: int) -> None:
+        self.head = ListNode(val, self.head)
+        self.size += 1
+
+    def addAtTail(self, val: int) -> None:
+        if not self.head:
+            self.addAtHead(val)
+            return
+        curr = self.head
+        while curr.next:
+            curr = curr.next
+        curr.next = ListNode(val)
+        self.size += 1
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        if index < 0 or index > self.size:
+            return
+        if index == 0:
+            self.addAtHead(val)
+            return
+        curr = self.head
+        for i in range(index-1):
+            curr = curr.next
+        curr.next = ListNode(val, curr.next)
+        self.size += 1
+
+    def deleteAtIndex(self, index: int) -> None:
+        if index < 0 or index >= self.size:
+            return
+        if index == 0:
+            self.head = self.head.next
+            self.size -= 1
+            return
+        curr = self.head
+        for i in range(index-1):
+            curr = curr.next
+        curr.next = curr.next.next
+        self.size -= 1
+        
+
+
+# Your MyLinkedList object will be instantiated and called as such:
+# obj = MyLinkedList()
+# param_1 = obj.get(index)
+# obj.addAtHead(val)
+# obj.addAtTail(val)
+# obj.addAtIndex(index,val)
+# obj.deleteAtIndex(index)
+
+
+
+
+
+# https://leetcode.com/problems/design-linked-list/
