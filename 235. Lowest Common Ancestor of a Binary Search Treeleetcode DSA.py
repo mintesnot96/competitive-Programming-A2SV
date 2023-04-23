4888cf6c00a856a3377f8a1ac0cbe88ebd461fe7
Mintesnot Fikir <96992238+mintesnot96@users.noreply.github.com>
2023-04-23 20:26:44 +0300

    Create 235. Lowest Common Ancestor of a Binary Search Treeleetcode DSA.py

---
+++ 235. Lowest Common Ancestor of a Binary Search Treeleetcode DSA.py
@@ -0,0 +1,34 @@
+# https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+
+        # Using recursion
+        if p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left,p,q)
+        elif p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right,p,q)
+        else:
+            return root
+
+        # while root:
+        #     if p.val < root.val and q.val < root.val:
+        #         root = root.left
+        #     elif p.val > root.val and q.val > root.val:
+        #         root = root.right
+        #     else:
+        #         return root
+
+
+         
+                
+
+
