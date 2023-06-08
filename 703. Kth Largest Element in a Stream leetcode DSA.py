cbc3b901b0ca1b4fcf2944320f3fb69aa7e61393
Mintesnot Fikir <96992238+mintesnot96@users.noreply.github.com>
2023-06-08 05:17:33 +0300

    Create 703. Kth Largest Element in a Stream leetcode DSA.py

---
+++ 703. Kth Largest Element in a Stream leetcode DSA.py
@@ -0,0 +1,16 @@
+import heapq
+class KthLargest:
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.min_heap = nums
+        heapq.heapify(self.min_heap)
+        while len(self.min_heap) > k:
+            heapq.heappop(self.min_heap)
+    def add(self, val: int) -> int:
+        if len(self.min_heap) < self.k:
+            heapq.heappush(self.min_heap, val)
+        elif val > self.min_heap[0]:
+            heapq.heappushpop(self.min_heap, val)
+        return self.min_heap[0]
+
+# https://leetcode.com/problems/kth-largest-element-in-a-stream/description/
