91eb040d58671a96396b60ed89314cb4ef22c320
Mintesnot Fikir <96992238+mintesnot96@users.noreply.github.com>
2023-06-22 19:56:03 +0300

    Create 137. Single Number II leetcode DSA.py

---
+++ 137. Single Number II leetcode DSA.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/single-number-ii/
+class Solution:
+  def singleNumber(self, nums: List[int]) -> int:
+    ones = 0
+    twos = 0
+
+    for num in nums:
+      ones ^= num & ~twos
+      twos ^= num & ~ones
+
+    return ones
