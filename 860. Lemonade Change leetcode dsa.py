f0fd946fc4ef4c60c10b2676005c10ff65e1203a
Mintesnot Fikir <96992238+mintesnot96@users.noreply.github.com>
2023-07-13 21:48:58 +0300

    Create 860. Lemonade Change leetcode dsa.py

---
+++ 860. Lemonade Change leetcode dsa.py
@@ -0,0 +1,21 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        five = ten = 0
+        for bill in bills:
+            if bill == 5:
+                five += 1
+            elif bill == 10:
+                if five == 0:
+                    return False
+                five -= 1
+                ten += 1
+            else:
+                if ten > 0 and five > 0:
+                    ten -= 1
+                    five -= 1
+                elif five >= 3:
+                    five -= 3
+                else:
+                    return False
+        return True
+# https://leetcode.com/problems/lemonade-change/description/
