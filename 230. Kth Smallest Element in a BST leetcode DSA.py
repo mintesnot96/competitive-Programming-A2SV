dd602bec003e42e154fa536decee4491bbfb20f1
Mintesnot Fikir <96992238+mintesnot96@users.noreply.github.com>
2023-07-19 23:03:34 +0300

    Create 230. Kth Smallest Element in a BST leetcode DSA.py

---
+++ 230. Kth Smallest Element in a BST leetcode DSA.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+class Solution:
+  def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+    def countNodes(root: Optional[TreeNode]) -> int:
+      if not root:
+        return 0
+      return 1 + countNodes(root.left) + countNodes(root.right)
+
+    leftCount = countNodes(root.left)
+
+    if leftCount == k - 1:
+      return root.val
+    if leftCount >= k:
+      return self.kthSmallest(root.left, k)
+    return self.kthSmallest(root.right, k - 1 - leftCount)  # LeftCount < k
