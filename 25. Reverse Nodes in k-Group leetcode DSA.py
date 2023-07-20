1a68b08cf735a3100b36228fdb289f0e7154a577
Mintesnot Fikir <96992238+mintesnot96@users.noreply.github.com>
2023-07-20 20:36:06 +0300

    Create 25. Reverse Nodes in k-Group leetcode DSA.py

---
+++ 25. Reverse Nodes in k-Group leetcode DSA.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/reverse-nodes-in-k-group/
+class Solution:
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+        if head is None or k < 2:
+            return head
+        dummy = ListNode(0)
+        dummy.next = head
+        start = dummy
+        end = head
+        count = 0
+        while end:
+            count += 1
+            if count % k == 0:
+                start = self.reverse(start, end.next)
+                end = start.next
+            else:
+                end = end.next
+        return dummy.next
+
+    def reverse(self, start, end):
+        prev, curr = start, start.next
+        first = curr
+        while curr != end:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        start.next = prev
+        first.next = curr
+        return first
