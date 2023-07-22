f93f9ec0233c0c58172306f9c9526ea40b86013d
Mintesnot Fikir <96992238+mintesnot96@users.noreply.github.com>
2023-07-22 20:45:25 +0300

    Create 1846. Maximum Element After Decreasing and Rearranging leetcode DSA.py

---
+++ 1846. Maximum Element After Decreasing and Rearranging leetcode DSA.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/maximum-element-after-decreasing-and-rearranging/
+class Solution:
+    def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
+        arr.sort()
+        arr[0] = 1
+        for i in range(1, len(arr)):
+            arr[i] = min(arr[i], arr[i - 1] + 1)
+        return arr[-1]
