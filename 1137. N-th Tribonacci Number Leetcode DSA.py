44b975dc017f4729631c923f4588d574ff4e9755
Mintesnot Fikir <96992238+mintesnot96@users.noreply.github.com>
2023-07-28 14:33:42 +0300

    Update 1137. N-th Tribonacci Number Leetcode DSA.py

--- 1137. N-th Tribonacci Number Leetcode DSA.py
+++ 1137. N-th Tribonacci Number Leetcode DSA.py
@@ -1,17 +1,17 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        
-        memo = [-1] * (n + 1)
-        def tri(n: int) -> int:
-            if n <= 0:
+        memo=[-1]*(n+1)
+
+        def dp(n):
+            if n<=0:
                 return 0
-            elif n == 1 or n == 2:
+            elif n==1 or n==2:
                 return 1
-            elif memo[n] != -1:
+            elif memo[n]!=-1:
                 return memo[n]
-
-            memo[n] = tri(n - 1) + tri(n - 2) + tri(n - 3)
+            memo[n]=dp(n-1)+dp(n-2)+dp(n-3)
             return memo[n]
+        return dp(n)
 
-        return tri(n)
+
 # https://leetcode.com/problems/n-th-tribonacci-number/description/
